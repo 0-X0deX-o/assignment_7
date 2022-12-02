ce934abc79b9ae314a94ee05028ee36cdb11d066
Rage <91225758+0-X0deX-o@users.noreply.github.com>
2022-12-02 11:24:10 -0600

    outline program structure

---
+++ brain_storm.md
@@ -0,0 +1,38 @@
+I find myself using a lot of time to plan:
+    brainstorm questions:
+        how can i save time each day
+            automatically create a list of lists that is segmented into 24 elements that is changes to when something is filled in
+    list of todos each day that are essentially constant each day
+    a way to check off whether or not the todo was accomplished
+        completion based productivity stats
+            period per ananlysis?
+            output to csv
+    incorporate productivity ideas
+        how much time did you actually spend in one place doing what activity?
+        -calorie, macro input calculator
+        -output calculator
+    
+    outputs the amount of time remaining in a day
+
+    incorporate data visualization for all meaningful formats
+
+    body composition stats tracker
+    vitals tracker
+
+        MR, Wake up routine, ER, sleep times, groceries, etc.
+
+store every workout as a list
+incorporate the python date module
+use the iCal python module
+use crons to send reminder emails (remember to turn on notifications)
+
+define 
+
+On second thought, thats too much intimate information into what my daily routine is.
+    develope a pygame for starters and see where that leads you
+
+Or create a modified calendar for turn in after you've perfected it for yourself
+
+<!-- 
+side note: find out who is responsible for grading in the upcoming classes to better prepare to complete assignments assignments  
+-->
---
+++ day_planner.py
@@ -0,0 +1,108 @@
+# this is due 12/9
+# D - 7 Days
+
+
+'''
+    Routine Constants
+'''
+todo_tasks = ['WR', 'MR','ER','Run','Boxing','Lecture','Abs','TOP', 'Boxing Practice', 'htb']
+sleep_hours = [8,4]
+available_hours = 24
+
+
+'''
+    Study specific...
+'''
+lectures = {
+    # class: [lecture title, text material covered]
+}
+
+
+
+
+
+
+
+'''
+    Fitness specifics
+'''
+run_log = {
+    # date/time: [mileage, pace, {BPM:[],pulse:[]}, (i/o)]
+} 
+   # percentage completion compared to plans/ expectation
+   # is there a way to figure out what the oxygen composition of the air is at what altitutes
+
+avg_run_pace = 9.2
+    # create a function that outputs the average pace, visualization of the progression  
+
+races = []
+    # figure out how to incorporate .ics files into when races are and develope a coutndown clock
+v02max = 0
+
+
+crossfit_log = {} 
+   # percentage completion compared to plans/ expectation
+
+strength_training_log = {} 
+   # percentage completion compared to plans/ expectation
+
+one_RM_log {
+    # date:[excercise,lift]
+}
+
+
+
+
+
+
+
+'''
+    Routine definintions
+
+'''
+WR_definition = '''
+3:45 - 4:00 Wake Up Routine
+   15 min wake up routine with boxing
+        -rnd 1-
+    2 min shadow boxing (warmup)
+    1 min rest
+        30 sec down 32 ounces water
+        30 sec crunches
+        -rnd 2-
+    2 min sh1adow boxing
+    1 min rest
+        30 sec get dressed
+        30 sec pushups
+        -rnd 3-
+    2 min shadow boxing (shoe shine rnd)
+    1 min rest
+        30 sec prep steak
+        30 sec crunches
+        -rnd 4-
+    2 min shadow boxing (1/2 hand speed round)
+    1 min rest
+        30 sec prep audio clip for run
+         30 sec crunches
+        -rnd 5-
+    2 min shadow boxing (constant defense)
+    1 min rest
+        prep for run
+            '''
+MR_definition  = '''
+2h
+    7.5 VR1
+    15 minute programming challenge (listen to the Bible)
+    5 memorization: Psalm 119 4vv.
+    7.5 Rosary
+    7.5 Prayer
+    15 morning hygiene
+        1m hot towel
+        8m shave
+        2m shower
+        4m moisturize, teeth, other hygine
+            30 min strictly reading eloquent javascript
+            22.5 VR2 
+1h
+    45 minute programming challenge while watching bloomberg to prep for the trading day
+    15 minute prep for the day
+       news articles in the new york times, wall street journal, pack for the day, lecture prep
